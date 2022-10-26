fb6fbcbc663ca53d24bbd474dd00b3372599890e
Martin Padron <martinepadronv@gmail.com>
2022-10-26 12:28:07 -0400

    Hero listo

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import style from "./Hero.module.scss";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import illus from "../../public/img/illustration.svg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={style.section}>
 
-export default Hero
+      <div className={style.container}>
+        <div className={style.img_container}>
+          <Image src={illus} alt="jmj taxes" />
+        </div>
+
+        <div className={style.contenido}>
+          <h1>Somos preparadores de documentos y formularios migratorios</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta fugit
+            ducimus hic veritatis vitae vel cumque? Laboriosam optio iusto at
+            suscipit velit aliquid maiores voluptatum?
+          </p>
+          <div className={style.btn_group}>
+            <Link href="#" className={style.btn1}> Btn 1</Link>
+            <Link href="#" className={style.btn2}> Btn 2</Link>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
