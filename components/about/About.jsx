8c4953978e2147d3c7af1297c615ab8266ffdff0
Martin Padron <martinepadronv@gmail.com>
2022-10-28 18:37:40 -0400

    Agregando el container de los emojis

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from "./About.module.scss";
 
+import { BiMedal } from "react-icons/bi";
+import { BiHappyBeaming } from "react-icons/bi";
+import { HiOutlineDocumentText } from "react-icons/hi";
+
 const About = () => {
   return (
-    <section className={style.section}>
+    <section className={style.section} id="about_section">
       <div className={style.container}>
         <div className={style.title}>
           <small>Sobre nosotros</small>
@@ -15,6 +19,40 @@ const About = () => {
             doloribus, voluptatum aperiam quaerat nesciunt et.
           </p>
         </div>
+
+        <div className="grid">
+          <div className="item">
+            <div className="header">
+              <BiMedal className="icon" />
+              <h4>+3</h4>
+            </div>
+            <div className="body">
+              <h4>Años de experiencia</h4>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="header">
+              <BiHappyBeaming className="icon" />
+              <h4>+25</h4>
+            </div>
+            <div className="body">
+              <h4>Trabajos exitosos</h4>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="header">
+              <HiOutlineDocumentText className="icon" />
+              <h4>+5</h4>
+            </div>
+            <div className="body">
+              <h4>Formas de trabajo</h4>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
